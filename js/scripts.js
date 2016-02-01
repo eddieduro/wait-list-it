66549fbed1aa2c4a3d11dfac56de8436b352a79e
michaellor <michael.vang.1985@gmail.com>
2016-02-01 14:15:45 -0800

    spec5: expaned to included wait time with array list number

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,9 @@
+var customers = [];
+
 function Restaurant(restaurantName) {
   this.restaurantName = restaurantName;
   this.customerList = [];
 }
-
-Restaurant.prototype.restaurantList = function() {
-  for (var i = 0; i < customers.length; i++) {
-    this.customerList.push(customers[i]);
-  }
-  console.log(this.customerList);
-  return this.customerList;
-}
-
-var customers = [];
 
 function Customer(customerName, phoneNumber, partySize) {
   this.customerName = customerName;
@@ -19,23 +11,42 @@ function Customer(customerName, phoneNumber, partySize) {
   this.partySize = partySize;
 }
 
+Restaurant.prototype.restaurantList = function() {
+  for (var i = 0; i < customers.length; i++) {
+    this.customerList.push(customers[i]);
+  }
+  return this.customerList;
+}
 
 Customer.prototype.customerInfo = function() {
   customers.push([this.customerName,this.phoneNumber,this.partySize]);
-  console.log(customers);
   return customers;
 }
 
 Customer.prototype.waitTime = function() {
   var parties = [];
   var time = 0;
+
   for(var i=0; i < customers.length; i++) {
-    parties.push(customers[i][2])
-    }
+    parties.push(customers[i][2]);
     console.log(parties);
-    for(var k=0; k < parties.length; k++) {
-      if (parties[k] === 2) {
-        return 30;
-      }
     }
+
+  for(var k=0; k < parties.length; k++) {
+    if (parties[k] === 2 || parties[k] === 1) {
+      time = 3;
+    }
+    if ((parties[k] > 2) &&  (parties[k] <= 4)) {
+      time = 5;
+    }
+    if ((parties[k] > 4 )&& (parties[k] <= 6)) {
+      time = 7;
+    }
+    if (parties[k] > 7) {
+      time = 9;
+    }
+      time += i * 5;
+      console.log(time);
+      return time;
+  }
 }
