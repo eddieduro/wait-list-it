c249730189a05792b1f07e8bf04dae2011a20c27
Eddie Duro and Michael Lor <student@epicodus.com>
2016-02-02 11:34:11 -0800

    static search bar functionality

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,6 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Imperial PDX", "text": "", "tags": "Asian, Imperial", "url": "/imperial.html"},
+     {"title": "Boxer Ramen", "text": "Boxer", "tags": "Asian, ramen, pdx", "url": "/index.html"},
+     {"title": "Pok Pok | PDX", "text": "", "tags": "pok pok, asian, chicken", "url": "/pokpok.html"}
+]};
