695cdc59a7a050c84e2529cef9db01a1b8b7c349
eddie <e.jamesduro@gmail.com>
2016-02-04 10:56:00 -0800

    yelp api

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
 app.listen(3000, function() { console.log('listening')});
+
+
 // var app = http.createServer(function(request, response){
 // 	fs.readFile(__dirname + "boxer.html", "utf-8", function(err, data){
 // 		response.writeHead(200, {'Content-Type': 'text/html'});
