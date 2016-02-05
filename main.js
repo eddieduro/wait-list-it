43e6fc4d18e21cf3231b6c4cc0e60ce72c226e50
eddieduro <e.jamesduro@gmail.com>
2016-02-05 08:44:13 -0800

    node engine

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/public'));
-app.listen( process.env.PORT || 3000, function() { 
+app.listen(process.env.PORT || 3000, function() { 
 	console.log('listening')
 });
 
